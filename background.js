f6e4a7cb2eed3d1a5ea70d865f45d91a91095c4c
osadan <sadanoh@gmail.com>
2014-02-20 18:23:05 +0200

    first commit

---
+++ background.js
@@ -0,0 +1,23 @@
+function getBgColors (tab) {
+	alert('the browser action was clicked1 yey!');
+}
+
+// Called when the url of a tab changes.
+function checkForValidUrl(tabId, changeInfo, tab) {
+  // If the letter 'g' is found in the tab's URL...
+  chrome.tabs.query({
+	'active' : true,
+	/*'windowId' : chrome.windows.WINDOW_ID_CURRENT*/
+	currentWindow:true
+  },function (tabs){
+  		if (tabs[0].url.indexOf('timetoknow') > -1) {
+		    // ... show the page action.
+		    chrome.pageAction.show(tabId);
+  		}
+  });
+  
+};
+
+// Listen for any changes to the URL of any tab.
+chrome.tabs.onUpdated.addListener(checkForValidUrl);
+//chrome.browserAction.onClicked.addListener(getBgColors);
