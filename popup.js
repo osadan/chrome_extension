30358fad734d1cb490a072af31836656a3e7c558
osadan <sadanoh@gmail.com>
2014-02-24 18:51:30 +0200

    save to localstorage

--- popup.js
+++ popup.js
@@ -1,14 +1,26 @@
 window.addEventListener("DOMContentLoaded", function() {
 	console.log('dom ready');
 	var button = document.getElementById('button');
+	var name = document.getElementById('name');
+	var password = document.getElementById('password');
+
+	if(localStorage['name']){
+		name.value = localStorage['name'];
+	}
+
+	if(localStorage['password']){
+		password.value = localStorage['password'];
+	}
+
 	button.addEventListener('click',function(){
 		//alert('click');
 		//console.log('click');
-		
 
 		var data = {};
 		data.name = document.getElementById('name').value;
 		data.password = document.getElementById('password').value;
+		localStorage['name'] = data.name;
+		localStorage['password'] = data.password;
 		
 		chrome.tabs.query({
         	active: true,
@@ -18,7 +30,7 @@ window.addEventListener("DOMContentLoaded", function() {
         	chrome.tabs.sendMessage(
                 tabs[0].id,
                 data);
-    });
+    	});
 
 
 
