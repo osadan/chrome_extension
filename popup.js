441f0b15a846d5fe2fa523171025961d607d8b1d
osadan <sadanoh@gmail.com>
2014-02-24 18:19:42 +0200

    yes! transmition success

--- popup.js
+++ popup.js
@@ -4,9 +4,24 @@ window.addEventListener("DOMContentLoaded", function() {
 	button.addEventListener('click',function(){
 		//alert('click');
 		//console.log('click');
+		
+
 		var data = {};
 		data.name = document.getElementById('name').value;
 		data.password = document.getElementById('password').value;
-		chrome.runtime.sendMessage(data);
+		
+		chrome.tabs.query({
+        	active: true,
+        	currentWindow: true
+		}, function(tabs) {
+        	/* ...and send a request for the DOM info... */
+        	chrome.tabs.sendMessage(
+                tabs[0].id,
+                data);
+    });
+
+
+
+		chrome.tabs.sendMessage(data);
 	});
 });
